d86ade0e9e1822c769772585a6538d59ce491bcc
Juanjo <juasangar11@alu.edu.gva.es>
2025-06-06 12:02:38 +0200

    Delete RecuProyecto directory

--- RecuProyecto/RecuProyecto/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.recuproyecto {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.recuproyecto to javafx.fxml;
-    exports com.example.recuproyecto;
-}
